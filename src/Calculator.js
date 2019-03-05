3626f5d93b9e4dbc259d8e89a91ffa30bdaa3124
metho <metholk@gmail.com>
2019-03-05 23:34:08 +0530

    skelaton of the app

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,7 +4,28 @@ import React, { Component } from 'react'
   render() {
     return (
       <div>
-        test
+        <form>
+          <div>
+            <label>Number 1 : </label>
+            <input/>
+            </div>
+            <div>
+            <label>Number 2 :</label>
+            <input/>
+            </div>
+            <div>
+              <button> + </button>
+              <button> - </button>
+              <button> * </button>
+              <button> / </button>
+              <button> % </button>
+              <button> C </button>
+            </div>
+            <div>
+              <h2></h2>
+            </div>
+         
+        </form>
       </div>
     )
   }
